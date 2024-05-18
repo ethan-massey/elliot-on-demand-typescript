e987a9692e0ad25fc8574f1d6bde8afdc82ba4e8
Ethan Massey <ejm4aa@virginia.edu>
2024-05-18 11:10:45 -0400

    add date to query params

--- src/handler/index.ts
+++ src/handler/index.ts
@@ -4,13 +4,13 @@ import { EpisodeEntity } from "../model/Episode.Entity";
 const express = require("express");
 export const defaultRouter = express.Router();
 
-defaultRouter.get("/episodes/:date?", async (request: any, response: any) => {
+defaultRouter.get("/episodes", async (request: any, response: any) => {
   try {
     const episodeRepository = new EpisodeRepository();
     let episodes: EpisodeEntity[] = [];
 
-    if (request.params.date) {
-      episodes = await episodeRepository.findByDate(request.params.date);
+    if (request.query.date) {
+      episodes = await episodeRepository.findByDate(request.query.date);
     } else {
       episodes = await episodeRepository.findAll();
     }
