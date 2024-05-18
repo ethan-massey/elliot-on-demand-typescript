ec03e1b6797df7afd62c48acf33230907e8cc012
Ethan Massey <ejm4aa@virginia.edu>
2024-05-18 10:19:07 -0400

    small edit

--- src/datasource/repositories/Episode.Repository.ts
+++ src/datasource/repositories/Episode.Repository.ts
@@ -10,8 +10,6 @@ export class EpisodeRepository extends MongoBaseRepository<EpisodeEntity> {
 
   public async findByDate(date: string): Promise<EpisodeEntity[]> {
     const query = await this.model.find({ date: date }).exec();
-    return query.map((item) => {
-      return item.toObject();
-    });
+    return this.convertDocumentsToObjects(query)
   }
 }
